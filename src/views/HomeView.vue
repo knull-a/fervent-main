154102922395382ae7000eef92e5c624bf1d06bf
knull-a <kenjopa6@gmail.com>
2025-04-06 11:10:51 +0500

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">Home</h1>
+  <Button>Click me</Button>
+</template>
